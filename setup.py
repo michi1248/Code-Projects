7ab4ff18a10cc5eb8cfda4ab2a7955122d41a170
Michael <michael.maerz@fau.de>
2022-11-27 15:05:55 +0100

    updating project structure to enabling pip install the project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='Code-Projects',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Michael März',
+    description='Some programming projects I have been working on',
+    url='https://github.com/michi1248/Code-Projects',
+)
