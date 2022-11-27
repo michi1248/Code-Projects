0052430a3832ac445dd05aa7bb87285ea1c3bfda
Michael <michael.maerz@fau.de>
2022-11-27 22:03:43 +0100

    updating project structure to enabling pip install the project

--- setup.py
+++ setup.py
@@ -9,4 +9,5 @@ setup(
     description='Some programming projects I have been working on',
     install_requires = ['numpy','turtles'],
     url='https://github.com/michi1248/Code-Projects',
+    options={"bdist_wheel": {"python_tag": "py3"}},
 )
