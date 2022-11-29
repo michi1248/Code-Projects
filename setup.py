e3e26e5838828efaf1360b72f41dc32bee0a14ff
Michael <michael.maerz@fau.de>
2022-11-29 08:50:07 +0100

    updating project structure to enabling pip install the project

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup,find_packages
+from setuptools import find_packages
+from distutils.core import setup
 
 
 setup(
-    name='Code-Projects',
-    version='1.0.0',
-    py_modules=['snowflake'],
-    #packages=find_packages(),
+    name='snowflake',
+    version='0.1',
+    #py_modules=['snowflake'],
+    packages=find_packages(),
+    #scripts='let_it_snow-py',
     author='Michael März',
     description='Some programming projects I have been working on',
     install_requires = ['numpy','turtles'],
     url='https://github.com/michi1248/Code-Projects',
-    options={"bdist_wheel": {"python_tag": "py3"}},
+    #options={"bdist_wheel": {"python_tag": "py3"}},
 )
--- snowflake/__init__.py
+++ snowflake/__init__.py
@@ -1,1 +1,1 @@
-from .let_it_snow import main
+from .let_it_snow import *
